9aeb12620bd84e1f1b88fe62122b3eff988c4bef
Neo Yin <yinzhang@berkeley.edu>
2024-05-21 13:32:00 -0700

    resnext50

--- scripts/train.py
+++ scripts/train.py
@@ -27,7 +27,7 @@ num_workers = 20
 downsample_factor = 1
 batch_size = 256
 img_size = 96
-num_classes = 2
+num_classes = 23
 
 ############################################################################
 ####### FUNCTIONS FOR DATA AUGMENTATION AND DATA LOADING ###################
