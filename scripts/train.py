d184b397deb644a18d6a64b7c4fdfe1c38c9ab0e
Neo Yin <yinzhang@berkeley.edu>
2024-05-21 11:06:59 -0700

    resnext50

--- scripts/train.py
+++ scripts/train.py
@@ -19,8 +19,8 @@ from torch.utils.data import WeightedRandomSampler
 ####### DEFINE HYPERPARAMETERS AND DATA DIRECTORIES ########################
 ############################################################################
 
+num_epochs = 500
 default_config = {"lr": 3.56e-07}  # 1.462801279401232e-06}
-num_epochs = 500  # 200
 data_dir = "/media/hdd1/neo/pooled_deepheme_data"
 num_gpus = 3
 num_workers = 20
@@ -209,9 +209,7 @@ class Myresnext50(pl.LightningModule):
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.config["lr"])
-        scheduler = CosineAnnealingLR(
-            optimizer, T_max=self.config["num_epochs"], eta_min=0
-        )
+        scheduler = CosineAnnealingLR(optimizer, T_max=num_epochs, eta_min=0)
         return {"optimizer": optimizer, "lr_scheduler": scheduler}
 
     def validation_step(self, batch, batch_idx):
