a374dbd27dc3c192a0bc1a982e2745719f43d9d5
Neo Yin <yinzhang@berkeley.edu>
2024-05-21 22:30:21 -0700

    deepheme retrain script

--- scripts/train.py
+++ scripts/train.py
@@ -20,7 +20,7 @@ from torch.utils.data import WeightedRandomSampler
 ############################################################################
 
 num_epochs = 500
-default_config = {"lr": 3.56e-07}  # 1.462801279401232e-06}
+default_config = {"lr": 3.56e-06}  # 1.462801279401232e-06}
 data_dir = "/media/hdd1/neo/pooled_deepheme_data"
 num_gpus = 3
 num_workers = 20
