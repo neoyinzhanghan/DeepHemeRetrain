226e3b016da40a5db327be7b3f3be725beb9d71b
Neo Yin <yinzhang@berkeley.edu>
2024-10-05 21:51:12 -0700

    model inference speed eval script

--- scripts/measure_inference_speed.py
+++ scripts/measure_inference_speed.py
@@ -11,15 +11,65 @@ result_dirs = [d for d in all_subdirs if "BMA-diff" in d or "PBS-diff" in d]
 
 all_result_dir_paths = [os.path.join(data_dir, d) for d in result_dirs]
 
+cellnames = [
+    "B1",
+    "B2",
+    "E1",
+    "E4",
+    "ER1",
+    "ER2",
+    "ER3",
+    "ER4",
+    "ER5",
+    "ER6",
+    "L2",
+    "L4",
+    "M1",
+    "M2",
+    "M3",
+    "M4",
+    "M5",
+    "M6",
+    "MO2",
+    "PL2",
+    "PL3",
+    "U1",
+    "U4",
+]
+
+def get_all_cell_paths(result_dir_path):
+    # first get all the subdirectories of result_dir_path/cells
+    cell_dir = os.path.join(result_dir_path, "cells")
+    all_cells = [d for d in os.listdir(cell_dir) if os.path.isdir(os.path.join(cell_dir, d))]
+
+    # only keep the ones that are in cellnames
+    cell_paths = [os.path.join(cell_dir, d) for d in all_cells if d in cellnames]
+
+    all_cell_img_paths = []
+
+    # get all the image paths in each cell path
+    for cell_path in cell_paths:
+        img_paths = [os.path.join(cell_path, f) for f in os.listdir(cell_path) if f.endswith(".jpg")]
+        all_cell_img_paths.extend(img_paths)
+
+    return all_cell_img_paths
+
 
 num_errors = 0
 num_dirs = len(all_result_dir_paths)
 non_error_dirs = []
+
+all_cell_paths = []
 for result_dir_path in tqdm(all_result_dir_paths, desc="Filtering out error dirs:"):
     # check if the result_dir_path contains a file called "error.txt"
     if not os.path.exists(os.path.join(result_dir_path, "error.txt")):
         non_error_dirs.append(result_dir_path)
+
+        cell_paths = get_all_cell_paths(result_dir_path)
+        all_cell_paths.extend(cell_paths)
     else:
         num_errors += 1
 
 print(f"Number of error directories: {num_errors} among {num_dirs} directories.")
+print(f"Number of non-error directories: {len(non_error_dirs)}")
+print(f"Number of cell image paths: {len(all_cell_paths)}")
