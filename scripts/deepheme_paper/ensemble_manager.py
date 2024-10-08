02c6a50eba90fb9a85da072497a9fc820643078e
Neo Yin <yinzhang@berkeley.edu>
2024-10-08 16:42:21 -0700

    max GPU memory used during inference

--- scripts/deepheme_paper/ensemble_manager.py
+++ scripts/deepheme_paper/ensemble_manager.py
@@ -2,7 +2,7 @@ import ray
 from train import model_create, model_predict, model_predict_batch
 
 
-@ray.remote(num_gpu=1)
+@ray.remote(num_gpus=1)
 class EnsembleManager:
     """Manages the ensemble of models.
 
